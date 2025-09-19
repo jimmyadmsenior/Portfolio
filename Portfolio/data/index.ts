3ebd6b6a8030be405a414372c85552537f336bd8
Jimmy Castilho <jimmycastilho555@gmail.com>
2025-09-19 08:57:48 -0300

    main changes related to the change of information

---
+++ Portfolio/data/index.ts
@@ -0,0 +1,187 @@
+export const navItems = [
+  { name: "Sobre", link: "#about" },
+  { name: "Projetos", link: "#projects" },
+  { name: "Experiência", link: "#experience" },
+  { name: "Contato", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Priorizo colaboração e comunicação aberta em todos os projetos",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Localizado em Salto, SP - Brasil",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Minha stack tecnológica",
+    description: "Sempre buscando evoluir",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Entusiasta em cibersegurança e desenvolvimento seguro.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Desenvolvendo ferramentas de segurança e sistemas robustos",
+    description: "Projetos em Andamento",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Quer começar um projeto juntos?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "SecureFileEncryptionTool",
+    des: "Ferramenta de criptografia de arquivos segura com interface gráfica. Permite criptografar e descriptografar arquivos de maneira fácil e segura.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://github.com/jimmyadmsenior/SecureFileEncryptionTool",
+  },
+  {
+    id: 2,
+    title: "Jimmy's AI - Chatbot",
+    des: "Chatbot desenvolvido utilizando a API do Google Gemini e JavaScript Vanilla para conversas inteligentes e interativas.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://github.com/jimmyadmsenior/Jimmy-s-AI",
+  },
+  {
+    id: 3,
+    title: "PasswordStrengthChecker",
+    des: "Verificador de força de senhas interativo e educacional que ajuda usuários a criar senhas mais seguras através de feedback em tempo real.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://github.com/jimmyadmsenior/PasswordStrengthChecker",
+  },
+  {
+    id: 4,
+    title: "MundoSENAI - Plataforma de Gestão",
+    des: "Plataforma de gestão de usuários e produtos desenvolvida para o curso de Desenvolvimento de Sistemas do SENAI.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/jimmyadmsenior/MundoSENAI-Front",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "python",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "javascript",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "react",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "flutter",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Desenvolvimento Full Stack",
+    desc: "Criação de sistemas web completos utilizando PHP, JavaScript, HTML/CSS e bancos de dados MySQL.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Cibersegurança e Ferramentas de Segurança",
+    desc: "Desenvolvimento de ferramentas de criptografia e verificação de segurança para proteger dados e sistemas.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Desenvolvimento de Aplicações Mobile",
+    desc: "Experiência com Flutter e Dart para criação de aplicativos móveis multiplataforma.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Sistemas e Infraestrutura",
+    desc: "Conhecimento em Linux, Docker e arquitetura de sistemas em nuvem para deploy de aplicações.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/jimmyadmsenior",
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/jimmycastilho/",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/jimmy-castilho-003733270/",
+  },
+];
